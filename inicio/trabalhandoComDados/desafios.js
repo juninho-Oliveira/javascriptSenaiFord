20b562017208c96a0934e4a01cfba17b015cadb9
junior <junior.oliveira05@icloud.com>
2023-11-25 13:48:04 -0300

    add exercicio

--- inicio/trabalhandoComDados/desafios.js
+++ inicio/trabalhandoComDados/desafios.js
@@ -8,23 +8,58 @@
 // tendo em mente que o raio da circunferência é de R = 16
 
 
-const Pi = 3.14;
-let R = 16
-const resul = R * R
-console.log(resul*Pi)
+// const Pi = 3.14;
+// let R = 16
+// const resul = R * R
+// console.log(resul*Pi)
 
-function areaCirculo (Pi, R) {
-  let Raio = R * R
-  return Raio * Pi
-}
+// function areaCirculo (Pi, R) {
+//   let Raio = R * R
+//   return Raio * Pi
+// }
 
-console.log(areaCirculo(Pi, R))
+// console.log(areaCirculo(Pi, R))
 
+// ************************************************ Atividade 02 **********************************************************
 
-// 1- Crie um algoritmo que calcule a idade de uma pessoa em 
+// 1 - Crie um algoritmo que calcule a idade de uma pessoa em 
 // a) Meses
 // b) Dias
 // c) Horas
 // d) Minutos
+
+const dataNascimento = new Date('1998-02-24')
+console.log(dataNascimento)
+
+const dataAtual = new Date();
+console.log(dataAtual)
+
+
+const diferencaEmMilissegundos = dataAtual - dataNascimento;
+const diferencaEmSegundos = diferencaEmMilissegundos / 1000;
+const diferencaEmMinutos = diferencaEmSegundos / 60;
+const diferencaEmHoras = diferencaEmMinutos / 60;
+const diferencaEmDias = diferencaEmHoras / 24;
+const diferencaEmMeses = diferencaEmDias / 30;
+
+const anos = Math.floor(diferencaEmDias / 365);
+const meses = Math.floor(diferencaEmMeses);
+const dias = Math.floor(diferencaEmDias % 365);
+const horas = Math.floor(diferencaEmHoras % 24);
+const minutos = Math.floor(diferencaEmMinutos % 60);
+
+
+console.log(`Idade: ${anos} anos, ${meses} meses, ${dias} dias, ${horas} horas, ${minutos} minutos`);
+
+
+
+
+
+
+
+
+ 
+
+
 
 
